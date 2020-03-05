93cca0a8c2896db2a891e39ac05b79dc43e7f92b
nicephil <nicephil@gmail.com>
2020-03-05 22:27:00 +0800

    restart always

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run -tid --name music -p 264:264 -v /home/llwang/repos/dockers/music-player-docker/cache:/var/www/html/cache oldiy/music-player-docker
+docker run -tid --restart=always --name music -p 264:264 -v /home/llwang/repos/dockers/music-player-docker/cache:/var/www/html/cache oldiy/music-player-docker
 
